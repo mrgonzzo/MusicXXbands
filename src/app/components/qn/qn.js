ae67aaf382c951b3c2d8f845b81b7a2fa232d813
mrgonzzo <mr.gonzzo@gmail.com>
2017-10-04 10:59:15 +0200

    ver Ok

--- src/app/components/qn/qn.js
+++ src/app/components/qn/qn.js
@@ -13,7 +13,7 @@ function controllerCompQueen ($state){
   var vm = this;
   vm.qn = { txt:'Queen es una banda británica de rock formada en 1970 en Londres por el cantante Freddie Mercury, el guitarrista Brian May, el baterista Roger Taylor y el bajista John Deacon. Si bien el grupo ha presentado bajas de dos de sus miembros (Mercury, fallecido en 1991, y Deacon, retirado en 1997), los integrantes restantes, May y Taylor, continúan trabajando bajo el nombre Queen, por lo que la banda aún es considerada activa',
   img:'app\resources\Queen.jpg',
-  video:'https://www.youtube.com/watch?v=5-zh0a6nDTo'
+  video:'5-zh0a6nDTo'
 };
 }
 
